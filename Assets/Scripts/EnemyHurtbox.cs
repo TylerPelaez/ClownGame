3e028dd227e151cedcbc692ffd813d4cc9e05d4c
Tyler Pelaez <mail@tylerpelaez.com>
2024-02-24 23:10:40 -0500

    Add Enemy AI

--- Assets/Scripts/EnemyHurtbox.cs
+++ Assets/Scripts/EnemyHurtbox.cs
@@ -1,9 +1,8 @@
-using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Enemy : MonoBehaviour
+public class EnemyHurtbox : MonoBehaviour
 {
     [SerializeField]
     private LayerMask layerMask;
