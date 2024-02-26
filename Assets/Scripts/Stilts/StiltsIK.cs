67ca55f04ba7d18acb62d868493704c9259b7b5b
Tyler Pelaez <mail@tylerpelaez.com>
2024-02-25 20:58:50 -0500

    Add stomping to stilts

--- Assets/Scripts/Stilts/StiltsIK.cs
+++ Assets/Scripts/Stilts/StiltsIK.cs
@@ -16,23 +16,19 @@ public class StiltsIK : MonoBehaviour
         {
             Vector3 leftBonePosition = leftFootBone.position - new Vector3(0f, transform.localScale.y/2.0f - offsetY, 0f);
 
-            // Calculate the difference in position between the current positions of the stilts and the bones
-            Vector3 positionDifference = leftBonePosition - leftStilt.position;
-
             // Apply the position difference to the left stilt
-            leftStilt.position += positionDifference;
+            leftStilt.position = leftBonePosition;
+
         }
 
         // Right foot
         if (rightFootBone != null)
         {
-            Vector3 rightBonePosition = rightFootBone.position - new Vector3(0f, transform.localScale.y/2.0f - offsetY, 0f);
-
-            // Calculate the difference in position between the current positions of the stilts and the bones
-            Vector3 positionDifference = rightBonePosition - rightStilt.position;
+            var position = rightFootBone.position;
+            Vector3 rightBonePosition = position - new Vector3(0f, transform.localScale.y/2.0f - offsetY, 0f);
 
             // Apply the position difference to the right stilt
-            rightStilt.position += positionDifference;
+            rightStilt.position = rightBonePosition;
         }
     }
 }
