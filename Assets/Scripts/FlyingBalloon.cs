76d98860c10c6a13208539e0f0e5f64de1f93781
Tyler Pelaez <mail@tylerpelaez.com>
2024-02-25 23:06:20 -0500

    Add Floating Ballon

---
+++ Assets/Scripts/FlyingBalloon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingBalloon : MonoBehaviour
+{
+    public void Pop()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Pie.cs
+++ Assets/Scripts/Pie.cs
@@ -1,29 +1,37 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Pie : MonoBehaviour
 {
+    [SerializeField]
+    private bool unmoving = false;
+    
     [SerializeField]
     private float speed = 100f;
 
     [SerializeField]
     private LayerMask destroyMask;
     
-
+    public UnityEvent OnHit;
+    
     private void Start()
     {
-        StartCoroutine(AwaitDeath());
+        if (!unmoving)
+            StartCoroutine(AwaitDeath());
     }
 
     private void FixedUpdate()
     {
-        transform.position += transform.forward * speed * Time.deltaTime;
+        if (!unmoving)
+            transform.position += transform.forward * speed * Time.deltaTime;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (destroyMask == (destroyMask | (1 << other.gameObject.layer)))
         {
+            OnHit?.Invoke();
             Destroy(gameObject);
         }
     }
